08d76489fbc9f4d5a63104d6720872ce8bb76cfd
Vinay Panchal <65890217+vinaynpp@users.noreply.github.com>
2021-09-14 14:00:01 +0530

    guides and instructions iframe

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from flask import Flask, render_template, request, redirect, url_for, session, j
 
 from werkzeug.datastructures import ImmutableMultiDict
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates', static_folder='static')
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 corehost = "https://cdn-api.co-vin.in/api/"
@@ -89,18 +89,27 @@ def getvp():
     if request.method == 'POST':
         param = {}
         stateres = getres(url="v2/admin/location/states", param=param)
-        districtres = getres(url="v2/admin/location/districts/"+ str(session["stateid"]) , param=param)
         
         session["pincode"] = request.form['pincode']
         session["date"] = dateconvert( request.form['date'])
         param = {"pincode": str(session["pincode"]), "date": str(session["date"])}
         pinres = getres(url="v2/appointment/sessions/public/findByPin", param=param)
         print(session)
-        return render_template("index.html",vaccine_details=pinres["sessions"], mydate = datereconvert(session["date"]), mypincode = session["pincode"], states=stateres["states"], mystateid = session["stateid"], districts=districtres["districts"], mydistrictid = session["districtid"])
+        return render_template("index.html",vaccine_details=pinres["sessions"], mydate = datereconvert(session["date"]), mypincode = session["pincode"], states=stateres["states"])
         
+@app.route('/instr', methods=['GET', 'POST'])
+def instr():
+    return render_template('instr.html')
 
+@app.route('/dos', methods=['GET', 'POST'])
+def dos():
+    return render_template('dos.html')
+
+@app.route('/about', methods=['GET', 'POST'])
+def about():
+    return render_template('about.html')
         
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(threaded=True, port=port, debug=True)
+    app.run(threaded=True, port=port, debug=True, host="0.0.0.0")
